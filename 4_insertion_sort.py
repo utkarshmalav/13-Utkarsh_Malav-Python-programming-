61a229fb0567a17f42159e773403b0d6e73e80aa
Utkarsh Baburao Malav <129851802+utkarshmalav@users.noreply.github.com>
2023-08-07 01:27:01 +0530

    Create 4_insertion_sort.py

---
+++ 4_insertion_sort.py
@@ -0,0 +1,12 @@
+# Write a program for sorting the numbers declared in the list using insertion sort.
+
+numb = [12,2.2,2.001,11,3,5,6,0]
+print("Unsorted list : "numb)
+for i in range(1, len(numb)):
+    key = numb[i]
+    j = i - 1
+    while j >= 0 and key < numb[j]:
+      numb[j + 1] = numb[j]
+      j -= 1
+    numb[j + 1] = key
+print("Sorted list : "numb)
