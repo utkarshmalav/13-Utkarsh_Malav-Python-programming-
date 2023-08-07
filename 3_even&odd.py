81148c2fceae66aad064225f1457e1bb0ae26f63
Utkarsh Baburao Malav <129851802+utkarshmalav@users.noreply.github.com>
2023-08-07 11:18:17 +0530

    3_even&odd.py

--- 3_even&odd.py
+++ 3_even&odd.py
@@ -1,4 +1,4 @@
-# Write a program for checking odd and even numbers from the tuple.
+# Write a program for checking odd and even numbers from the tuple. 
 
 numb= (1, 2, 3, 4, 5, 6, 7, 8, 9,10,121,155,242)
 
