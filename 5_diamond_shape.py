07ad6fe88dd3d70ba69a33ebbf0a8506398cb73f
Utkarsh Baburao Malav <129851802+utkarshmalav@users.noreply.github.com>
2023-08-14 08:06:50 +0530

    Create 5_diamond_shape.py

---
+++ 5_diamond_shape.py
@@ -0,0 +1,21 @@
+# Converted an given c pgm into python of hollow diamond pattern.
+
+def print_hollow_diamond(n):
+    for rows in range(1, n + 1):
+        print(" " * (n - rows), end="")
+        print("*", end="")
+        if rows != 1:
+            print(" " * ((rows - 1) * 2 - 1), end="")
+            print("*", end="")
+        print()
+
+    for rows in range(n - 1, 0, -1):
+        print(" " * (n - rows), end="")
+        print("*", end="")
+        if rows != 1:
+            print(" " * ((rows - 1) * 2 - 1), end="")
+            print("*", end="")
+        print()
+
+n = 5
+print_hollow_diamond(n)
