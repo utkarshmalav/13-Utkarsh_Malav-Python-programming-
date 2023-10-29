48b0c644b684deab796ccac244133a1191472ca8
Utkarsh Baburao Malav <129851802+utkarshmalav@users.noreply.github.com>
2023-10-29 23:43:27 +0530

    Add files via upload

---
+++ basic_python.py
@@ -0,0 +1,51 @@
+
+#list_touple
+print("list_touple\n")
+x=[1,'harsh']
+for n in x:
+        print(n)
+x=[2,'uttu']
+for n in x:
+    print(n)
+
+y=(10,'vijay')
+for m in y:
+    print(m)
+
+y=(11,'joy')
+for h in y:
+    print(h)
+print()
+print()
+
+
+#set_example
+print("Set\n")
+set1 = set([1,2,3,3])
+set2 = set([1,2,3,4])
+print(set1)
+print(set2)
+print()
+print()
+
+
+#Mapping
+print("Mapping\n")
+dict1={1:"first",'a':"helo", 3:{'A':"hi",2:1},4:[1,2,3,4]}
+print(dict1['a'])
+print(dict1[1])
+print(dict1[3])
+print(dict1[4])
+print()
+print()
+
+#boolean
+print("boolean\n")
+b=True
+print(b)
+print(type(b))
+print()
+print()
+
+
+
---
+++ shot_hand_ifelse.py
@@ -0,0 +1,2 @@
+i=18
+print("Value is less than 15") if i<15 else print("Value is greater than 15")
