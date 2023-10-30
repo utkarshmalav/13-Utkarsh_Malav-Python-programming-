da0d2ebfeed7403e032b88a0aaf9bf94845f78f0
Utkarsh Baburao Malav <129851802+utkarshmalav@users.noreply.github.com>
2023-10-30 11:05:20 +0530

    writeFile.py

--- writeFile.py
+++ writeFile.py
@@ -1,6 +1,6 @@
 file=open("input.txt","w")
 w="hello\n"
-file.writelines(["I am Shruti Ugale\n","My Roll No is 4\n","I am student of DYPCET\n"])
+file.writelines(["I am Utkarsh Malav\n","My Roll No is 13\n","I am student of DYPCET\n"])
 print(file.write(w))
 
 file=open("input.txt","r")
